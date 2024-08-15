54d7eaca5537389b75ebdf01498d3b55e90ac01b
Tanyamodi <mtanu0126@gmail.com>
2024-08-15 13:14:02 +0530

    update

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -23,7 +23,7 @@ const CartItem = ({ item, itemIndex }) => {
           <h1 className="text-xl text-slate-700 font-semibold">{item.title}</h1>
           <h1 className="text-base text-slate-700 font-medium">{item.description}</h1>
           <div className="flex items-center justify-between">
-            <p className="font-bold text-lg text-green-600">{item.price}</p>
+            <p className="font-bold text-lg text-green-600">${item.price}</p>
             <div className="text-red-800  bg-red-200 group hover:bg-red-400 transition-transform duration-300 cursor-pointer rounded-full p-3 mr-3"
               onClick={removeFromCart}>
               <AiFillDelete />
